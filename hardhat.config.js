ccac6da222b6f83f643d660a953c686f4757c21d
John <njukijohn2000@gmail.com>
2024-04-29 03:08:44 +0300

    add dependencies

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,18 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    arbitrumSepolia: {
+      url: 'https://sepolia-rollup.arbitrum.io/rpc',
+      chainId: 421614,
+      //accounts: [process.env.SEPOLIA_TESTNET_PRIVATE_KEY],
+    },
+    arbitrumOne: {
+      url: 'https://arb1.arbitrum.io/rpc',
+      //accounts: [process.env.ARBITRUM_MAINNET_TEMPORARY_PRIVATE_KEY],
+    }
+  }
 };
